4bd14633d4972c20aa433983df8249399597b615
Shubh Prabhat <shubh.techie@gmail.com>
2021-11-13 14:56:12 -0500

    create new solution folder

---
+++ Geeksforgeeks/CompetitiveProgramming.Geeksforgeeks.ArrayDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompetitiveProgramming.Geeksforgeeks.ArrayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
