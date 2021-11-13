c2dc37d560a57c0efa71a5951aa9654950702c56
Shubh Prabhat <shubh.techie@gmail.com>
2021-11-13 15:12:10 -0500

    create new solution folder

---
+++ GeeksForGeeks/GeeksForGeeks.ArrayDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeeksForGeeks.ArrayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
