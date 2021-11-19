353999a6feef7486895555579f5028be94287ed8
Shubh Prabhat <shubh.techie@gmail.com>
2021-11-18 23:50:46 -0500

    updated

---
+++ GeeksForGeeks/GeeksForGeeks.QueueDemo/QueueHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeeksForGeeks.QueueDemo
+{
+    public class QueueHelper
+    {
+        public QueueHelper()
+        {
+            CSharpQueueDemo();
+        }
+
+        private void CSharpQueueDemo()
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(100);
+            queue.Dequeue();
+            queue.Peek();
+        }
+    }
+}
---
+++ GeeksForGeeks/GeeksForGeeks.StackDemo/MyStack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeeksForGeeks.StackDemo
+{
+    class MyStack<T>
+    {
+        T[] list;
+        int Size;
+        public int Count { get; set; }
+
+        public MyStack(int size)
+        {
+            this.Size = size;
+            this.Count = 0;
+            list = new T[this.Size];
+        }
+        public void Push(T no)
+        {
+            if (IsFull()) throw new Exception("Stack is overflow");
+            list[Size] = no;
+            Count++;
+        }
+
+        public T Pull()
+        {
+            if (IsEmpty()) throw new Exception("Stack is underflow.");
+            this.Count--;
+            T tempNumber = list[Count];
+            return tempNumber;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty()) throw new Exception("Stack is underflow.");
+            return list[Count];
+        }
+
+        public bool IsFull()
+        {
+            return this.Count == this.Size;
+        }
+
+        public bool IsEmpty()
+        {
+            return this.Size == 0;
+        }
+    }
+}
