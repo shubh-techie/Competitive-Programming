74e27902be8c9bd5e70e9aa4e321e60a8cd138ca
Shubh Prabhat <shubh.techie@gmail.com>
2022-05-06 23:11:21 -0400

    Added binary tree and binary search tree

---
+++ LeetCodeSolution/LeetCode.TreeDemo/TreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.TreeDemo
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val =0)
+        {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+}
